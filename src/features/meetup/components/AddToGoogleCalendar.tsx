dc7281ca585b6cc30804350ce4ac4ff3ae95eec6
coji <coji@techtalk.jp>
2022-06-09 12:13:32 +0900

    fixed hydration error

--- src/features/meetup/components/AddToGoogleCalendar.tsx
+++ src/features/meetup/components/AddToGoogleCalendar.tsx
@@ -1,17 +1,24 @@
+import { useState, useEffect } from 'react'
 import { Link, Button, Icon } from '@chakra-ui/react'
 import { BiCalendarPlus } from 'react-icons/bi'
 import dayjs from '~/libs/dayjs'
 import type { Meetup } from '../interfaces/meetup'
 
 export const AddToGoogleCalendar = ({ meetup }: { meetup: Meetup }) => {
-  const startAt = dayjs(meetup.startAt).format('YYYYMMDDTHHmm00')
-  const endAt = dayjs(meetup.endAt).format('YYYYMMDDTHHmm00')
-  const dates = `${startAt}/${endAt}`
-  const location = `${meetup.locationLabel}`
-  const description = encodeURI(
-    `${meetup.description}\n\nYamatoji\n${window.location.href}`
-  )
-  const link = `https://www.google.com/calendar/render?action=TEMPLATE&text=${meetup.title}&dates=${dates}&details=${description}&location=${location}`
+  const [link, setLink] = useState<string>('')
+
+  useEffect(() => {
+    const startAt = dayjs(meetup.startAt).format('YYYYMMDDTHHmm00')
+    const endAt = dayjs(meetup.endAt).format('YYYYMMDDTHHmm00')
+    const dates = `${startAt}/${endAt}`
+    const location = `${meetup.locationLabel}`
+    const description = encodeURI(
+      `${meetup.description}\n\nYamatoji\n${window.location.href}`
+    )
+    setLink(
+      `https://www.google.com/calendar/render?action=TEMPLATE&text=${meetup.title}&dates=${dates}&details=${description}&location=${location}`
+    )
+  }, [meetup])
 
   return (
     <Link href={link} isExternal _hover={{ textDecoration: 'none' }}>
